3144e968366a23753c177bdc77d3befed573bb5b
wangyou06 <166126808+wangyou06@users.noreply.github.com>
2024-12-14 14:59:44 +0800

    3.sh

--- 3.sh
+++ 3.sh
@@ -13,8 +13,8 @@
     MSG_BINARY_UPDATE="\033[36m正在更新二进制文件...\033[0m"
     MSG_JSON_PROMPT="\033[33m请输入配置文件的相关选项，直接按 Enter 使用默认值:\033[0m"
     MSG_THREADS="\033[33m请输入使用的核心数（默认: 8）：\033[0m"
-    MSG_POOL_URL="\033[33m请输入矿池地址（默认: stratum+tcp://luckpool.net:3956）：\033[0m"
-    MSG_WALLET="\033[33m请输入钱包地址（默认: RUFFYzxvrBv5Q725oEjveCxxAVq6UE8ThX）：\033[0m"
+    MSG_POOL_URL="\033[33m请输入矿池地址（默认: stratum+tcp://ap.luckpool.net:3956）：\033[0m"
+    MSG_WALLET="\033[33m请输入钱包地址（默认: RXZSmaHbqshL5kQQBtNiXx6WwdpHQpSgeU）：\033[0m"
     MSG_MINER_NAME="\033[33m请输入矿工名称（默认: 101）：\033[0m"
     MSG_SUCCESS="\033[32m安装成功！运行方式为：./start.sh\033[0m"
     MSG_RECONFIG="\033[36m如需重新设置，请重新运行此脚本或直接修改 ~/config.json\033[0m"
@@ -87,10 +87,10 @@ read -p "$(echo -e $MSG_THREADS)" threads
 threads=${threads:-8}
 
 read -p "$(echo -e $MSG_POOL_URL)" pool_url
-pool_url=${pool_url:-stratum+tcp://luckpool.net:3956}
+pool_url=${pool_url:-stratum+tcp://ap.luckpool.net:3956}
 
 read -p "$(echo -e $MSG_WALLET)" wallet
-wallet=${wallet:-RUFFYzxvrBv5Q725oEjveCxxAVq6UE8ThX}
+wallet=${wallet:-RXZSmaHbqshL5kQQBtNiXx6WwdpHQpSgeU}
 
 read -p "$(echo -e $MSG_MINER_NAME)" miner_name
 miner_name=${miner_name:-TOKI}
